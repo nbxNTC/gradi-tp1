81fc4387fea751a803ab2159e64dd6d50184f078
Matheus <matheus12bh@gmail.com>
2021-02-23 12:42:12 -0300

    add-exercises-table

---
+++ server/src/database/migrations/1614094598679-create-exercises.ts
@@ -0,0 +1,59 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createExercises1614094598679 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'exercises',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'integer',
+                    unsigned: true,
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+                },
+                {
+                    name: 'title',
+                    type: 'varchar'
+                },
+                {
+                    name: 'observation',
+                    type: 'varchar'
+                },
+                {
+                    name: 'series',
+                    type: 'decimal'
+                },
+                {
+                    name: 'repetitions',
+                    type: 'decimal'
+                },
+                {
+                    name: 'rest',
+                    type: 'decimal'
+                },
+                {
+                    name: 'equipment_id',
+                    type: 'integer'
+                }
+            ],
+            foreignKeys: [
+                {
+                    name: 'ExerciseEquipment',
+                    columnNames: ['equipment_id'],
+                    referencedTableName: 'equipments',
+                    referencedColumnNames: ['id'],
+                    onUpdate: 'CASCADE',
+                    onDelete: 'CASCADE'
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('exercises');
+    }
+
+}
