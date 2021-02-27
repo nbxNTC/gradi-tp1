a1f1d4af8f48097d355268ce86fab7cd1476192f
Mario Sergio <39653977+nbxNTC@users.noreply.github.com>
2021-02-27 16:42:12 -0300

    chore: add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,14 @@
-import express from 'express';
+import express from 'express'
+import cors from 'cors'
 
+import './database/connection'
+import routes from './routes'
 
-import './database/connection';
+const app = express()
 
-import routes from './routes';
-
-const app = express();
-
-app.use(express.json());
-app.use(routes);
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 
 app.listen(3333)
